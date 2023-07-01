a48771275e341cbfd4c7f7dee5fbd41b16a97032
Aleksey Kuzmin <kboomska@gmail.com>
2023-07-01 12:19:17 +0300

    TheRickAndMorty: Presentation. Adding scroll controller

--- rick_and_morty/lib/feature/presentation/pages/person_screen.dart
+++ rick_and_morty/lib/feature/presentation/pages/person_screen.dart
@@ -19,7 +19,7 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: const PersonsList(),
+      body: PersonsList(),
     );
   }
 }
--- rick_and_morty/lib/feature/presentation/widgets/persons_list_widget.dart
+++ rick_and_morty/lib/feature/presentation/widgets/persons_list_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,16 +10,34 @@ import 'package:rick_and_morty/feature/presentation/widgets/person_card_widget.d
 import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
 
 class PersonsList extends StatelessWidget {
-  const PersonsList({super.key});
+  final scrollController = ScrollController();
+
+  PersonsList({super.key});
+
+  void setupScrollController(BuildContext context) {
+    scrollController.addListener(() {
+      if (scrollController.position.atEdge) {
+        if (scrollController.position.pixels != 0) {
+          context.read<PersonListCubit>().loadPerson();
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    setupScrollController(context);
+
     return BlocBuilder<PersonListCubit, PersonState>(
       builder: (context, state) {
         List<PersonEntity> persons = [];
+        bool isLoading = false;
 
         if (state is PersonLoading && state.isFirstFetch) {
           return _loadingIndicator();
+        } else if (state is PersonLoading) {
+          persons = state.oldPersonsList;
+          isLoading = true;
         } else if (state is PersonLoaded) {
           persons = state.personsList;
         } else if (state is PersonError) {
@@ -32,14 +52,24 @@ class PersonsList extends StatelessWidget {
           );
         }
         return ListView.separated(
-          itemCount: persons.length,
+          controller: scrollController,
+          itemCount: persons.length + (isLoading ? 1 : 0),
           separatorBuilder: (context, index) {
             return Divider(
               color: Colors.grey[400],
             );
           },
           itemBuilder: (context, index) {
-            return PersonCard(person: persons[index]);
+            if (index < persons.length) {
+              return PersonCard(person: persons[index]);
+            } else {
+              Timer(const Duration(milliseconds: 30), () {
+                scrollController.jumpTo(
+                  scrollController.position.maxScrollExtent,
+                );
+              });
+              return _loadingIndicator();
+            }
           },
         );
       },
